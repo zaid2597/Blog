274f3e657a834e5b5c3ae06770895c8fc185d76d
zaid2597 <141477364+zaid2597@users.noreply.github.com>
2026-02-20 00:21:48 +0500

    debugging hogae

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Navbar from "./components/website/layout/Navbar";
 import Footer from "./components/website/layout/Footer";
 import HeroSection from "./components/website/home/HeroSection";
@@ -6,8 +7,12 @@ import ContentSections from "./components/website/home/ContentSections";
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <HeroSection />
+      <Suspense fallback={null}>
+        <Navbar />
+      </Suspense>
+      <Suspense fallback={null}>
+        <HeroSection />
+      </Suspense>
       {/* <ContentSections />  */}
       <Footer />
     </>
